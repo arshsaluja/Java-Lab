934a0b6d1ee9c4706db4134cc34e5629d99e902c
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-01-25 22:17:35 +0530

    add 2 nos using command line arguments

---
+++ Experiment-2/Add_c.java
@@ -0,0 +1,8 @@
+public class Add_c {
+public static void main( String args[] ) {
+ int num= Integer.parseInt(args[0]);
+ int num1= Integer.parseInt(args[1]);
+
+  System.out.println(num+ num1);
+   }
+}
