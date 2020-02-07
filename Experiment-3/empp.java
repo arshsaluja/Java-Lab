f0d9ca0175f2a5518c4ebab601ee69ef67f5f3c1
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-02-07 11:39:59 +0530

    employee and  manager

---
+++ Experiment-3/empp.java
@@ -0,0 +1,59 @@
+import java.util.*;
+class employee
+{
+String name;
+int empid;
+int salary;
+employee()
+{
+name="bajaj";
+empid=67;
+salary=9000;
+}
+employee(String n,int e,int s)
+{
+name=n;
+empid=e;
+salary=s;
+}
+String no()
+{
+return name;
+}
+int fo()
+{
+return salary;
+}
+double increaseSalary(double per_in)
+{
+double in=(salary*(per_in/100));
+double s=salary+in;
+return s;
+}
+}
+class manager extends employee
+{
+public String department="d";
+
+}
+class emppp 
+{
+public static void main(String args[])
+{
+Scanner sc= new Scanner(System.in);
+System.out.println("enter the name");
+String n=sc.next();
+System.out.println("enter the employee id");
+int e=sc.nextInt();
+System.out.println("enter the salary");
+int s=sc.nextInt();
+System.out.println("enter the percentage_rate increaced in salary");
+double p=sc.nextDouble();
+employee m=new employee(n,e,s);
+System.out.println(m.no());
+System.out.println(m.fo());
+System.out.println(m.increaseSalary(p));
+}
+}
+
+
