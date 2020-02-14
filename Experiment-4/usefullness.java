1cf6de037c563ed0acfd31cda9bf25bffe937623
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-02-14 11:00:23 +0530

    //constant value of program //importance of interfaces

---
+++ Experiment-4/usefullness.java
@@ -0,0 +1,15 @@
+//constant value of program
+//importance of interfaces
+interface i
+{
+int total=200;
+}
+class exp5_3 implements i
+{
+	public static void main(String args[])
+	{
+exp5_3 m= new exp5_3();
+
+System.out.println(m.total=100);
+	}
+}
