4029fc0a4dc55c5193fc43dd1411cbdab014e4d1
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-02-26 21:18:28 +0530

    Create exp5_1.1.java

---
+++ Experiment -5/exp5_1/exp5_1.1.java
@@ -0,0 +1,10 @@
+//implement the concept of importing classes from user defined package
+//and created packages
+package p2;
+public class exp6_1
+{
+public void fun()
+{
+System.out.println("fun in package p2");
+}
+}
