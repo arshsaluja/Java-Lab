1a34b97e8695b4973932e183ad8e316f36539b38
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-02-26 21:43:54 +0530

    Create exp5_1_2.java

---
+++ Experiment -5/exp5_1/exp5_1_2.java
@@ -0,0 +1,14 @@
+package adp2;
+import p2.exp6_1;
+class adp21{
+public static void main(String args[])
+{
+exp6_1 e= new exp6_1();
+e.fun();
+}
+}
+//to compile
+//javac exp6_1.java
+//javac adp21.java
+//java adp2.adp21
+//output--  fun in package p2
