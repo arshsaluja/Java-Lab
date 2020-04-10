44d31e3f19f3ca217e99c934f194e429fe980a67
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-04-11 00:55:38 +0530

    Wrapper Classes

---
+++ Experiment-8/exp9_4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class exp9_4
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+//4.>    converts int to  String 
+System.out.println("Enter an int value ");
+int p = sc.nextInt();
+String  k= Integer.toString(p);
+System.out.println("Converted int to String is   "+ k);
+}
+}
