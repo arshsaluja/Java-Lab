1b3ec7cd23b4311829ccdd064dc76a051d192992
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-04-11 00:57:45 +0530

    Create exp9_5.java

---
+++ Experiment-8/exp9_5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class exp9_5
+{
+public static void main(String args[])
+{
+	Scanner sc=new Scanner (System.in);
+//5.>    converts String to Integer ,converts Integer to int.
+System.out.println("Enter a String value");
+String s2=sc.next();
+Integer t=Integer.valueOf(s2);
+Integer y=t;
+System.out.println("Converted String value to Wrapper Integer is   "+ y);
+}
+}
