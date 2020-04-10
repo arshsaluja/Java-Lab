8d0548253e75f5fd5760c15ace88e91d771da399
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-04-11 01:17:50 +0530

    //Wrapper Classes- converts Integer to int

--- Experiment-8/exp9_6.java
+++ Experiment-8/exp9_6.java
@@ -5,7 +5,7 @@ class exp9_6
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-//6.>    converts Integer to int
+//6.>    converts Integer to int.
 System.out.println("Enter an Integer value (Wrapper)");
 int n=sc.nextInt();
 Integer h=new Integer(n);
