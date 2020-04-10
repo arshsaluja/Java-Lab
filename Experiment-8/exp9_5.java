29fd30903b3ec8334d2278117f96b996f1a9ad63
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-04-11 01:17:13 +0530

    //Wrapper Classes-converts String to Integer

--- Experiment-8/exp9_5.java
+++ Experiment-8/exp9_5.java
@@ -4,7 +4,7 @@ class exp9_5
 public static void main(String args[])
 {
 	Scanner sc=new Scanner (System.in);
-//5.>    converts String to Integer ,converts Integer to int.
+//5.>    converts String to Integer 
 System.out.println("Enter a String value");
 String s2=sc.next();
 Integer t=Integer.valueOf(s2);
