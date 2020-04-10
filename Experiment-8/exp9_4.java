cca709ca925c65a888435fb5458143e254c97fb0
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-04-11 01:16:23 +0530

    //Wrapper- Classes-converts int to  String

--- Experiment-8/exp9_4.java
+++ Experiment-8/exp9_4.java
@@ -4,7 +4,7 @@ class exp9_4
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-//4.>    converts int to  String 
+//4.>    converts int to  String .
 System.out.println("Enter an int value ");
 int p = sc.nextInt();
 String  k= Integer.toString(p);
