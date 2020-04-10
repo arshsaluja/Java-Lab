bf89d80fb9a9c7b2fb589d1d7bad714b7c220422
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-04-11 01:03:15 +0530

    //Wrapper Classes- converts String to int

--- Experiment-8/exp9_3.java
+++ Experiment-8/exp9_3.java
@@ -4,10 +4,10 @@ class exp9_3
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-//3.>    converts String to int
+//3.>    converts String to int.
 System.out.println("Enter the String value :");
 String str=sc.next();
-// we can use Integer.parseInt(str)  or Integer.valueOf(str)
+// we can use Integer.parseInt(str)  or Integer.valueOf(str).
 int r=Integer.valueOf(str);
 System.out.println("Converted String to primitive int is   "+ r);
 
