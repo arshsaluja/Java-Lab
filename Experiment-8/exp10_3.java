6591bd94977481ba961232c222751ece9083cc80
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-04-11 19:48:09 +0530

    //converts String to float

---
+++ Experiment-8/exp10_3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class exp10_3
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+//converts String to float
+System.out.println("Enter a String value that is converted to float ");
+String w=sc.next();
+float w1=Float.parseFloat(w);
+System.out.println(w1);
+}
+}
