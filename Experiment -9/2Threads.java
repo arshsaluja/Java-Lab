77fe21df45bf5814ebbcf6ad1b9512c075b1044e
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-06-27 22:20:35 +0530

    Create 2Threads.java

---
+++ Experiment -9/2Threads.java
@@ -0,0 +1,28 @@
+//2)	Write a program for generating 2 threads, one for printing even numbers and the other for printing odd numbers.
+
+class even extends Thread{
+   public void run(){
+        for(int i=0;i<10;i++){
+            if(i%2==0)
+            System.out.println(i+ "even");
+        }
+    }
+}
+class odd extends Thread{
+   public void run(){
+        
+        for(int i=0;i<10;i++){
+            if(i%2 !=0)
+            System.out.println(i + "odd");
+        }
+    }
+}
+public class thread7 {
+    public static void main(String[] args) {
+        even e=new even();
+        odd o=new odd();
+        e.start();
+        o.start();
+    }
+    
+}
